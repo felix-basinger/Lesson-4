9b6474846fdc578a34eeb028aed1a99b320a699b
Basinger <fel_simon@mail.ru>
2023-04-04 21:43:14 +0300

    Added Task002_BigSum

---
+++ Task002_BigSum/Program.cs
@@ -0,0 +1,26 @@
+﻿//  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+SumDig();
+
+void SumDig()
+{   
+    double num;
+    System.Console.Write("Enter your number: ");
+    if(double.TryParse(Console.ReadLine(), out num))
+    {   
+        if(num < 0) System.Console.WriteLine("Error! It's a negative number");
+        else
+        {
+            string str = num.ToString();
+            int count = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                int numb = Convert.ToInt32(Convert.ToString(str[i]));
+                count += numb;
+            }
+            System.Console.WriteLine($"Sum of digits in your number = {count}");
+        }
+        
+    }
+    else System.Console.WriteLine("It's not a number");
+}
