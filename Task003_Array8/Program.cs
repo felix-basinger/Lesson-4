7112e69b00dc4d395a49791b16784d165af703f9
Basinger <fel_simon@mail.ru>
2023-04-04 23:43:10 +0300

    Added Task003_Array8

---
+++ Task003_Array8/Program.cs
@@ -0,0 +1,21 @@
+﻿//  Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] array = new int[8];
+
+for (int i = 0; i < array.Length; i++)
+{   
+    System.Console.Write($"Enter the {i + 1} one or two-digit positive number: ");
+    array[i] = int.Parse(Console.ReadLine());
+    if(array[i] > 99 || array[i] < 0) 
+    {
+        while(array[i] > 99 || array[i] < 0)
+        {
+            System.Console.WriteLine("it's too big or negative number!");
+            System.Console.Write("Enter the one or two-digit positive number: ");
+            array[i] = int.Parse(Console.ReadLine());
+        }
+    }
+    else System.Console.Write("");    
+    // array[i] = new Random().Next(1,100);
+}
+Console.WriteLine(string.Join(", ",array));
